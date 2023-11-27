e305248de51d1735cede42435614e6f3fef9fefe
FilippoBersan <filippobersan99@gmail.com>
2023-11-27 10:33:39 +0100

    feat: aggiunta immagine

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ createApp({
   data() {
     return {
       message: 'Ciao classe 110, prova Vue Js',
+      ciao: 'sono elemento foto',
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3WaP8XZ0Z8Yw8jTuX7PD736bzDovzhXe7ZEFOCX4YecPAcADXOxVnWM9llXp3xHQ9vzU&usqp=CAU',
     };
   },
 }).mount('#app');
